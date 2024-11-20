9642348f8f8efea9a7f1be300aebbeff2a5dd8f3
Alistair Buckle <agtbuckle@gmail.com>
2024-11-20 19:56:21 +0000

    Init commit: TypeScript built from watch tasks using either rollup or tsc-watch.

---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+import run from "@rollup/plugin-run"
+import typescript from "rollup-plugin-typescript2"
+
+export default {
+  input: "src/index.ts",
+  output: {
+    dir: "dist-rollup",
+    format: "cjs",
+  },
+  plugins: [
+    typescript({
+      noEmitOnError: false,
+    }),
+    run(),
+  ],
+  watch: {
+    exclude: ["node_modules/**"],
+  },
+}
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { add, substract } from "./math/index.js"
+
+const x = 22
+const y = 11
+
+console.log(`${x} + ${y} = ${add(x, y)}`)
+console.log(`${x} - ${y} = ${substract(x, y)}`)
