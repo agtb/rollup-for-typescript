7964dd9921db58affdc0ed6901125cb6279038f4
Alistair Buckle <agtbuckle@gmail.com>
2024-11-20 20:32:04 +0000

    Fix typo substract -> subtract in demo code.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import { add, substract } from "./math/index.js"
+import { add, subtract } from "./math/index.js"
 
-const x = 22
+const x = 21
 const y = 11
 
 console.log(`${x} + ${y} = ${add(x, y)}`)
-console.log(`${x} - ${y} = ${substract(x, y)}`)
+console.log(`${x} - ${y} = ${subtract(x, y)}`)
