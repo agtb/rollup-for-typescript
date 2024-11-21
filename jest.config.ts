10dc58ddcfa8f9d9a141b712b442d91afe8d839f
Alistair Buckle <agtbuckle@gmail.com>
2024-11-21 19:52:13 +0000

    Add test coverage setup.

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,7 @@
 import type { JestConfigWithTsJest } from "ts-jest"
 
 const jestConfig: JestConfigWithTsJest = {
+  coverageReporters: ["html", "text", "text-summary", "cobertura"],
   moduleNameMapper: {
     "^@App/(.*)$": "<rootDir>/src/$1",
     "^lib/(.*)$": "<rootDir>/common/$1",
