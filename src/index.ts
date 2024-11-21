89530bf4825b3352f39b4bc640c57ff5f2be70ec
Alistair Buckle <agtbuckle@gmail.com>
2024-11-21 10:52:29 +0000

    Add string manipulation examples.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import { add, subtract } from "./math/index.js"
+import { toLowerCase, toUpperCase } from "./string/string.js"
 
 const x = 22
 const y = 11
 
 console.log(`${x} + ${y} = ${add(x, y)}`)
 console.log(`${x} - ${y} = ${subtract(x, y)}`)
+
+console.log(toLowerCase("HELLO WORLD"))
+console.log(toUpperCase("hello world"))
